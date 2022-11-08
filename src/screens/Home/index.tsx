c094d82397591c23f75cdf2a29f71a14397ec62e
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-08 14:43:13 -0300

    add loading with lottie

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -19,7 +19,7 @@ const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 import LogoSvg from '@assets/logo.svg';
 
 import { Car } from '@components/Car';
-import { Load } from '@components/Load';
+import { LoadAnimation } from '@components/LoadAnimation';
 
 import { api } from '../../services/api';
 import { CarDTO } from '@dtos/CarDTO';
@@ -116,7 +116,7 @@ export function Home() {
         </HeaderContent>
       </Header>
       
-      {loading ? <Load /> :
+      {loading ? <LoadAnimation /> :
         <FlatList
           data={cars}
           keyExtractor={item => item.id}
